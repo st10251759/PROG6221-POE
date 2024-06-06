61cc620d80943ea9d283d063ac6717d7ec90e467
st10251759 <ST10251759@vcconnect.edu.za>
2024-06-07 00:01:50 +0200

    Chanaged my manageRecepie class to work with wpf by removing all console coding

---
+++ ManageRecepie.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace ST10251759_PROG6221_POE
+{
+    public class ManageRecepie
+    {
+        public List<Recipe> Recipes { get; private set; }
+
+        public ManageRecepie()
+        {
+            Recipes = new List<Recipe>();
+        }
+
+        public void AddRecipe(Recipe recipe)
+        {
+            Recipes.Add(recipe);
+        }
+
+        public void DisplayAllRecipes(ListBox recipeListBox)
+        {
+            recipeListBox.Items.Clear();
+            var sortedRecipes = Recipes.OrderBy(r => r.Name).ToList();
+
+            foreach (var recipe in sortedRecipes)
+            {
+                recipeListBox.Items.Add(recipe.Name);
+            }
+        }
+
+        public void DisplayRecipeDetails(Recipe recipe, TextBlock recipeDetailsTextBlock)
+        {
+            recipeDetailsTextBlock.Text = recipe.DisplayRecipe();
+        }
+
+        public void ScaleRecipe(Recipe recipe, double factor)
+        {
+            recipe.ScaleRecipe(factor);
+        }
+
+        public void ResetRecipe(Recipe recipe)
+        {
+            recipe.ResetRecipe();
+        }
+
+        public void DeleteRecipe(Recipe recipe)
+        {
+            if (Recipes.Contains(recipe))
+            {
+                Recipes.Remove(recipe);
+                MessageBox.Show($"Recipe '{recipe.Name}' has been deleted.", "Delete Recipe", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show($"Recipe '{recipe.Name}' not found.", "Delete Recipe", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
