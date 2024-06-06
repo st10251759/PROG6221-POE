64564f7d76474323f157f7ec7f0c4d2ae93c6006
st10251759 <ST10251759@vcconnect.edu.za>
2024-06-06 22:45:25 +0200

    Migrated my exsisting ingredient class and step class as well as my enums from part 2

---
+++ Step.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Student Number: ST10251759
+ Full Name: Cameron Chetty
+ Course: PROG6221
+ Assessment: POE Part 2
+ Github Link for Part 1: https://github.com/VCDN-2024/prog6221-part-1-st10251759
+ Github Link for Part 2: https://github.com/st10251759/prog6221-part-2-st10251759
+ */
+
+/*
+=============Feedback==================== 
+I obtained 100% and reciped no iplemented feedback. All work done was to address the requirements of PART 2 ONLY, and there  NO FEEDBACK to implement for part 1.  
+*/
+
+/*
+=============Code Attribution====================
+
+Author: Fatima Shaik 
+Website: https://github.com/fb-shaik/PROG6221-Group1-2024/tree/main/Class_Objects_App
+Date of Access: 23 March 2024  
+
+Author: Fatima Shaik 
+Website: https://github.com/fb-shaik/PROG6221-Group1-2024/tree/main/Generics_Library_App
+Date of Access: 29 May 2024 
+
+Author: Fatima Shaik 
+Website: https://github.com/fb-shaik/PROG6221-Group1-2024/tree/main/Collection_Generics_LU3
+Date of Access: 29 May 2024 
+
+
+=============Code Attribution====================
+*/
+
+namespace ST10251759_PROG6221_POE
+{//namespace begin
+    internal class Step
+    {//Step class begin
+        //Get Set method of attribute Description in step class
+        public string Description { get; set; }
+    }//step class end
+}//namespace end
---
+++ UnitOfMeasurement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Student Number: ST10251759
+ Full Name: Cameron Chetty
+ Course: PROG6221
+ Assessment: POE Part 2
+ Github Link for Part 1: https://github.com/VCDN-2024/prog6221-part-1-st10251759
+ Github Link for Part 2: https://github.com/st10251759/prog6221-part-2-st10251759
+ */
+
+/*
+=============Feedback==================== 
+I obtained 100% and reciped no iplemented feedback. All work done was to address the requirements of PART 2 ONLY, and there  NO FEEDBACK to implement for part 1.  
+*/
+
+/*
+=============Code Attribution====================
+
+Author: Fatima Shaik 
+Website: https://github.com/fb-shaik/PROG6221-Group1-2024/tree/main/Class_Objects_App
+Date of Access: 23 March 2024 
+
+Author: Fatima Shaik 
+Website: https://github.com/fb-shaik/PROG6221-Group1-2024/tree/main/EnumDemo_App
+Date of Access: 09 April 2024 
+
+=============Code Attribution====================
+*/
+
+namespace ST10251759_PROG6221_POE
+{//naemspace begin
+    //Enum that has the units of measurement.  The Enum provides a set of predefined units that can be used to specify the quantity of ingredients in a recipe.
+    public enum UnitOfMeasurement
+    {//enum begin
+        SMALL,
+        MEDIUM,
+        LARGE,
+        EXTRALARGE,
+        TEASPOON,
+        TEASPOONS,
+        TABLESPOON,
+        TABLESPOONS,
+        CUP,
+        CUPS
+    }//enum end
+}//namespace end
